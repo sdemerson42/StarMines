13683eb001bc2a96794a537c300ab0276f15206b
Scott Emerson <semerson@blizzard.com>
2017-10-18 16:23:20 -0700

    Animator was returning instead of cycling through all active components. Fixed.

--- StarMines/Animator.cpp
+++ StarMines/Animator.cpp
@@ -12,22 +12,30 @@ void Animator::update()
 		auto &c = m_compManager->m_anim[i];
 		auto &a = c.m_curAnim;
 
-		if (!c.m_playing) return;
-
-		++c.m_counter;
-		if (c.m_counter < a.fDelay) return;
-
-		c.m_counter = 0;
-		if (++c.m_frame == a.frameTot)
+		if (c.m_playing)
 		{
-			if (!a.loop)
+			++c.m_counter;
+			if (c.m_counter == a.fDelay)
 			{
-				c.m_playing = false;
-				return;
+				c.m_counter = 0;
+				if (++c.m_frame == a.frameTot)
+				{
+					if (!a.loop)
+					{
+						c.m_playing = false;
+					}
+					else
+					{
+						c.m_frame = 0;
+					}
+				}
+				if (c.m_playing)
+				{
+					auto r = c.parent()->getComponent<RenderComponent>();
+					r->m_position = a.framePos[c.m_frame];
+				}
 			}
-			c.m_frame = 0;
 		}
-		auto r = c.parent()->getComponent<RenderComponent>();
-		r->m_position = a.framePos[c.m_frame];
+		++i;
 	}
 }
