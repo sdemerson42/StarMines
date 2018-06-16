a60fcaf0287e4947e5cb1fd9e4fcbeeb7f297fb5
SDE <sdemerson42@gmail.com>
2018-06-15 21:49:21 -0700

    Added a helper function to Main.lua to converts CSV strings to arrays.

--- StarMines/Data/Lua/Main.lua
+++ StarMines/Data/Lua/Main.lua
@@ -1,5 +1,21 @@
--- Behavior execution loop
+-- string to ary
+function stoary(s)
+	local index = 1
+	local r = {}
+	local i = string.find(s, ',')
+	while i ~= nil do
+		local vs = string.sub(s, 1, i-1)
+		r[index] = tonumber(vs)
+		s = string.sub(s, i + 1, -1)
+		index = index + 1
+		i = string.find(s, ',')
+	end
+	r[index] = tonumber(s)
+	r[index+1] = nil
+	return r
+end
 
+-- Behavior execution loop
 local count = getBCSize()
 for i = 0, count-1 do
 	local bc = getBC(i)
@@ -8,3 +24,7 @@ for i = 0, count-1 do
 	require(path)
 	_G[module](bc)
 end
+
+
+
+
